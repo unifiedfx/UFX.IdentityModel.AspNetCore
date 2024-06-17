abe72e8bc029e5ad1571714b8a6de7a55178fa01
Stephen Welsh <stephen.welsh@unifiedfx.com>
2024-06-17 19:17:38 +0100

    update build.csproj

--- build/Program.cs
+++ build/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using static Bullseye.Targets;
 using static SimpleExec.Command;
 
@@ -22,7 +23,7 @@ namespace build
             public const string SignPackage = "sign-package";
         }
 
-        internal static void Main(string[] args)
+        internal static async Task Main(string[] args)
         {
             Target(Targets.RestoreTools, () =>
             {
@@ -65,8 +66,8 @@ namespace build
             Target("default", DependsOn(Targets.Test, Targets.Pack));
 
             Target("sign", DependsOn(Targets.Test, Targets.SignPackage));
-
-            RunTargetsAndExit(args, ex => ex is SimpleExec.NonZeroExitCodeException || ex.Message.EndsWith(envVarMissing));
+            await RunTargetsAndExitAsync(args, ex => ex is SimpleExec.ExitCodeException || ex.Message.EndsWith(envVarMissing));
+            // RunTargetsAndExit(args, ex => ex is SimpleExec.NonZeroExitCodeException || ex.Message.EndsWith(envVarMissing));
         }
 
         private static void SignNuGet()
